2fa58b731b63af92a607d9b0e6409ca908f1db1f
Fritz Guzman <fritz.developer10@gmail.com>
2023-11-07 20:39:04 -0500

    actualizacion footer

--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -8,20 +8,18 @@ const Footer = () => {
         <div className="footer-title">Fritz</div>
         <div className="footer-box">
           <ul className="f-1">
-            <li>Ayudas</li>
+            <li>Ayuda</li>
             <li>Términos y condiciones</li>
             <li>Medios de Pago</li>
           </ul>
           <ul className="f-1">
             <li>Quienes Somos</li>
-            <li>Quienes Somos</li>
             <li>Nuestras Tiendas</li>
             <li>Facturación electrónica</li>
           </ul>
           <ul className="f-1">
-            <li>Contactanos</li>
-            <li>Escríbenos fritz.guzman.santiago@gmail.com</li>
-            <li>Teléfonos whatsapp: +51981659039</li>
+            <li>Escríbenos a: fritz.guzman.santiago@gmail.com</li>
+            <li>Teléfonos / whatsapp: +51-981659039</li>
           </ul>
         </div>
       </footer>
