16b26cf636eb40c27bd9d28f14bb61ab2ba8eb3f
Fritz <fritz.developer10@gmail.com>
2024-08-21 10:45:28 -0500

    nav box_shadon and position:stiky

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import NavBar from "../Components/NavBar/NavBar";
 import { Outlet } from "react-router-dom";
-import Footer from "../Components/Footer/Footer"
+import Footer from "../Components/Footer/Footer";
+
+import './Layout.css';
 
 const Layout = () => {
   return (
     <div>
-      <div style={{ height: "10vh" }}>
+      <div className="containerNav" style={{ height: "10vh" }}>
         <NavBar />
       </div>
       <div style={{ minHeight: "80vh" }}>
